5b92abc6365d5dec98eac952e8b317f89e025e3c
Austin Mackillop <austin.mackillop@gmail.com>
2018-12-28 18:13:31 -0500

    Build vocabs using multiprocessing

---
+++ machine-translation/src/preprocessing.py
@@ -0,0 +1,124 @@
+import os
+import math
+import multiprocessing as mp
+import collections
+import itertools as it
+import re
+import pickle
+
+import config
+import utils
+
+import nltk
+
+
+def process_file(fname, nprocesses=mp.cpu_count()):
+    """Process a file using multiprocessing."""
+    with open(fname, 'r', encoding = 'iso-8859-1') as text:
+        strands = utils.spagettify(text, nprocesses)
+        result = utils.parallelize(build_vocab, strands)
+        return result
+
+
+def build_vocab(lines, result_queue):
+    """Build a word count dictionary given an iterable of raw lines."""
+    vocab = collections.Counter()
+    chunks = utils.chunker(lines, 10000, '')
+    for chunk in chunks:
+        tokenizer = Tokenizer(chunk)
+        vocab.update(tok for tok in tokenizer)
+    result_queue.put(vocab)
+    return vocab
+
+
+def build_mapping(vocab, nwords):
+    return {word: str(i) for i, (word, _) in enumerate(vocab.most_common(nwords))}
+
+
+class Tokenizer(collections.abc.Generator):
+    tokenizer = nltk.tokenize.wordpunct_tokenize
+
+    def __init__(self, lines):
+        str_chunk = self._remove_punc(' '.join(lines)).replace('\n', '.')
+        self.token = None
+        self.tokens = iter(self.tokenizer(str_chunk))
+
+    def send(self, arg):
+        self.token = self._clean(next(self.tokens))
+        return self.token
+
+    def throw(self, type=None, value=None, traceback=None):
+        raise StopIteration
+
+    @staticmethod
+    def _clean(token):
+        if token.strip().isdigit():
+            return '#'
+        elif token.strip() == '.':
+            return '.'
+        return token.lower()
+
+    @staticmethod
+    def _remove_punc(string):
+        """Remove puncuation."""
+        return re.sub(r'[^\w\s]', '', string)
+
+    def _build_sentence(self):
+        while True:
+            try:
+                next(self)
+            except StopIteration:
+                return
+            yield self.token
+            if self.token == '.':
+                break
+
+    def yield_sentences(self, mapping=None):
+        """Generate sentences.
+
+        If a mapping is provided, the sentences will be translated as per the map.
+
+        """
+        if mapping is None:
+            def _map(x): return x
+        else:
+            def _map(x): return mapping.get(x, len(mapping))
+
+        while True:
+            sentence = ' '.join(str(_map(word)) for word in self._build_sentence())
+            if sentence:
+                yield sentence
+            else:
+                return
+
+
+# def translate_to_int(fname, mapping):
+#     with open(fname, encoding = 'iso-8859-1') as source:
+#         with open('processed/' + fname.rpartition('/')[-1], 'w') as output:
+#             chunks = utils.chunker(source, 10000, '')
+#             for chunk in chunks:
+#                 tokenizer = Tokenizer(chunk)
+#                 sentences = (sent for sent in tokenizer.yield_sentences(mapping))
+#                 for sentence in sentences:
+#                     output.write(sentence + '\n')
+
+
+def main():
+    """Build vocabularies if not present as a pickle file."""
+    datasets = 'datasets/'
+    vocabs = 'vocabs/'
+    source_files = [fname for fname in os.listdir(datasets)]
+    for source in source_files:
+        try:
+            vocab = utils.unpickle_data(f'{vocabs}{source}.pickle')
+        except FileNotFoundError:
+            results = process_file(datasets + source)
+            vocab = collections.Counter()
+            for result in results:
+                vocab.update(result)
+            utils.pickle_data(vocab, f'{vocabs}{source}.pickle')
+
+
+if __name__ == '__main__':
+    main()
+    pass
---
+++ machine-translation/src/utils.py
@@ -0,0 +1,82 @@
+import itertools as it
+import pickle
+import math
+import multiprocessing as mp
+
+
+def spagettify(iterable, strands):
+    return [it.islice(iterable, i, None, strands) for i in range(strands)]
+
+
+def pickle_data(data, fname):
+    with open(fname, 'wb') as pfile:
+        pickle.dump(data, pfile, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def unpickle_data(fname):
+    with open(fname, 'rb') as pfile:
+        data = pickle.load(pfile)
+    return data
+
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(it.islice(iterable, n))
+
+
+def chunker(iterable, chunksize, fillvalue=None):
+    """Yield `chunk_size` tuples of values from any iterable."""
+    args = [iter(iterable)] * chunksize
+    return it.zip_longest(*args, fillvalue=fillvalue)
+
+
+def parallelize(func, args_list, n_processes=None):
+    """Parallelize a function call across multiple processes.
+
+    This function will create equal length batches of the provided `args_list`
+    and kick off a process for each batch. If they cannot be split evenly
+    across the n processes then the last batch will be shorter. If they can be
+    split evenly across a lesser number of batches, then this will do that for
+    you.
+
+    For example, processing 6 files across 4 processes will just kick off 3
+    processes that do 2 files each as this is more efficient.
+
+    If you have to pass multiple arguments to your function, make sure that the
+    `args_list` is a list of tuples wth the arguments in the correct order.
+
+    One last note, the passed function must accept a multiprocessing.Queue()
+    object as the last argument and instead of returning, use
+    queue.put(<return value>) instead. This is how the function will determine
+    when all processes are complete and be able to return all of the results to
+    the parent process.
+
+    Args
+    ----
+    func : <function>
+        What function to call.
+    args_list : list
+        The list of arguments to batch and call in parallel.
+    n_processes : int
+        Number of processes to use. Defaults to system cpu count.
+
+    """
+    if n_processes is None:
+        n_processes = mp.cpu_count()
+    result_queue = mp.Queue()
+    batch_size = int(math.ceil(len(list(args_list)) / n_processes))
+    batched_args = chunker(args_list, batch_size)
+    processes = []
+    for batch in batched_args:
+        # Gets rid of Nones if this is the short batch
+        func_args = [arg for arg in batch if arg]
+        p = mp.Process(target=func, args=tuple([*func_args, result_queue]))
+        p.start()
+        processes.append(p)
+
+    result = [result_queue.get() for p in processes]
+
+    for i, p in enumerate(processes):
+        p.join()
+
+    return result
