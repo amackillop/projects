b47ca28ca03880b05c62c42f576be0baa1837572
Austin Mackillop <austin.mackillop@askuity.com>
2019-03-10 19:23:27 -0400

    some work done

--- scala-99/src/problems.scala
+++ scala-99/src/problems.scala
@@ -1,5 +1,6 @@
 import scala.annotation.tailrec
 
+
 object Main extends App {
   val result = Problems.penultimate(List(1))
   println(result)
@@ -28,13 +29,19 @@ object Problems {
   }
 
   //P04
-  def length[A](list: List[A]): Int = (0 /: list) { (len, _) => len + 1 }
+  def length[A](list: List[A]): Int = {
+    (0 /: list) { (len, _) => len + 1 }
+  }
 
   //P05
-  def reverse[A](list: List[A]): List[A] = (List[A]() /: list) { (xs, x) => x +: xs }
+  def reverse[A](list: List[A]): List[A] = {
+    (List[A]() /: list) { (xs, x) => x +: xs }
+  }
 
   //P06
-  def isPalindrome[A](list: List[A]): Boolean = reverse(list) == list
+  def isPalindrome[A](list: List[A]): Boolean = {
+    reverse(list) == list
+  }
 
   //P07
   def flatten(list: List[Any]): List[Any] = list flatMap {
@@ -77,7 +84,9 @@ object Problems {
   }
 
   //P10
-  def encode[A](list: List[A]): List[(Int, A)] = pack(list) map { (xs) => (xs.length, xs.head) }
+  def encode[A](list: List[A]): List[(Int, A)] = {
+    pack(list) map { (xs) => (xs.length, xs.head) }
+  }
 
   //P11
   def encodeModified[A](list: List[A]): List[Any] = pack(list) map {
@@ -85,7 +94,9 @@ object Problems {
   }
 
   //P12
-  def decode[A](list: List[(Int, A)]): List[A] = list flatMap { e => List.fill(e._1)(e._2) }
+  def decode[A](list: List[(Int, A)]): List[A] = {
+    list flatMap { e => List.fill(e._1)(e._2) }
+  }
 
   //P13
   def encodeDirect[A](list: List[A]): List[(Int, A)] = {
@@ -94,10 +105,14 @@ object Problems {
   }
 
   //P14
-  def duplicate[A](list: List[A]): List[A] = list flatMap { (x) => List(x, x) }
+  def duplicate[A](list: List[A]): List[A] = {
+    list flatMap { (x) => List(x, x) }
+  }
 
   //P15
-  def duplicateN[A](n: Int)(list: List[A]): List[A] = list flatMap { (x) => List.fill(n)(x) }
+  def duplicateN[A](n: Int)(list: List[A]): List[A] = {
+    list flatMap { (x) => List.fill(n)(x) }
+  }
 
   //P16
   def dropEveryNth[A](n: Int)(list: List[A]): List[A] = for {
@@ -105,24 +120,29 @@ object Problems {
   } yield x
 
   //P17
-  def split[A](n: Int)(list: List[A]): (List[A], List[A]) = (list take n, list drop n)
+  def split[A](n: Int)(list: List[A]): (List[A], List[A]) = {
+    (list take n, list drop n)
+  }
 
   def splitRecursive[A](n: Int)(list: List[A]): (List[A], List[A]) = {
     @tailrec
     def splitR(n: Int, left: List[A], right: List[A]): (List[A], List[A]) = (n, right) match {
       case (_, Nil) => (left.reverse, Nil)
-      case (0, _)   => (left.reverse, right)
-      case (n, _)   => splitR(n - 1, right.head :: left, right.tail)
+      case (0, _) => (left.reverse, right)
+      case (n, _) => splitR(n - 1, right.head :: left, right.tail)
     }
+
     splitR(n, Nil, list)
   }
 
   //P18
-  def slice[A](start: Int, stop: Int, list: List[A]): List[A] = (list drop start) take (stop - (start max 0))
+  def slice[A](start: Int, stop: Int, list: List[A]): List[A] = {
+    (list drop start) take (stop - (start max 0))
+  }
 
   //P19
   def rotate[A](n: Int)(list: List[A]): List[A] = {
-    if(n >= 0) {
+    if (n >= 0) {
       val (left, right) = list.splitAt(n)
       right ::: left
     } else {
@@ -133,8 +153,8 @@ object Problems {
 
   //P20
   def removeAt[A](n: Int)(list: List[A]): (List[A], Option[A]) = list.splitAt(n) match {
-    case (Nil, _)      => (list, None)
-    case (_, Nil)      => (list, None)
+    case (_, Nil) => (list, None)
+    case (Nil, right) => (right.tail, Some(right.head))
     case (left, right) => (left ::: right.tail, Some(right.head))
   }
 
@@ -146,12 +166,87 @@ object Problems {
   //P22
   def range(start: Int, stop: Int): List[Int] = {
     @tailrec def rng(stop: Int, list: List[Int]): List[Int] = {
-      if(stop < start) list else rng(stop - 1, stop :: list)
+      if (stop < start) list else rng(stop - 1, stop :: list)
     }
+
     rng(stop, Nil)
   }
 
   //P23
-  def
+  def randInt(below: Int) = {
+    scala.util.Random.nextInt(below)
+  }
+
+  // With resampling
+  def randSelect[A](n: Int)(xs: List[A]): List[A] = {
+    val limit = xs.length
+
+    @tailrec
+    def pickRand(n: Int, xs: List[A], picks: List[A]): List[A] = {
+      n match {
+        case 0 => picks
+        case n => {
+          val pick = xs(randInt(limit))
+          pickRand(n - 1, xs, pick :: picks)
+        }
+      }
+    }
+
+    pickRand(n, xs, Nil)
+  }
+
+  // Without resampling
+  def chooseN[A](n: Int)(xs: List[A]): List[A] = {
+    @tailrec
+    def pickRand[A](n: Int, xs: List[A], picks: List[A]): List[A] = {
+      (n, xs) match {
+        case (0, _)   => picks
+        case (_, Nil) => picks
+        case (n, xs)  => {
+          val (rest, pick) = removeAt(randInt(xs.length))(xs)
+          pickRand(n - 1, rest, pick.get :: picks)
+        }
+      }
+    }
+
+    pickRand(n, xs, Nil)
+  }
+
+  //P24
+  def lotto(choose: Int, from1to: Int): List[Int] = {
+    chooseN(choose)(range(1, from1to))
+  }
+
+  //P25
+  def randomPermute[A](xs: List[A]): List[A] = {
+    chooseN(xs.length)(xs)
+  }
+
+  //TODO: Implement a more efficient solution for P25
+
+  //P26
+//  def combinations[A](xs: List[A], chooseN: Int): List[List[A]] = (xs, chooseN) match {
+//    case (_, 0)  => List(Nil)
+//    case (xs, 1) => xs.map(List(_))
+//    case _ => List(Nil)
+//  }
+
+  def flatMapSublists[A,B](ls: List[A])(f: (List[A]) => List[B]): List[B] = {
+    @tailrec
+    def loop(ls: List[A], f: List[A] => List[B], accum: List[B]): List[B] =
+      ls match {
+        case Nil => accum
+        case sublist@(_ :: tail) => loop(tail, f, accum ::: f(sublist))
+      }
+
+    loop(ls, f, Nil)
+  }
+
+  def combinations[A](n: Int, ls: List[A]): List[List[A]] =
+    if (n == 0) List(Nil)
+    else flatMapSublists(ls) { sl =>
+      combinations(n - 1, sl.tail) map {sl.head :: _}
+    }
 
 }
+
